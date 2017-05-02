3cd7bbf07eb251ee3dc256520d0899297393ce22
Ken Rutan and Mohamed Warsame <mohamed@>
2017-05-02 10:20:55 -0700

    run next three spec tests successfully

--- spec/doctors_spec.rb
+++ spec/doctors_spec.rb
@@ -16,11 +16,32 @@ describe(Patient) do
   end
 
   describe('#save') do
-      it("adds a patient to the array of saved patients") do
-        test_patient = Patient.new({:name => "Mark Jones", :doctor_id => 1, :birthday => "2008-11-11"})
-        test_patient.save()
-        expect(Patient.all()).to(eq([test_patient]))
-      end
+    it("adds a patient to the array of saved patients") do
+      test_patient = Patient.new({:name => "Mark Jones", :doctor_id => 1, :birthday => "2008-11-11"})
+      test_patient.save()
+      expect(Patient.all()).to(eq([test_patient]))
     end
+  end
+
+  describe('#name') do
+    it("lets you view the patient's name") do
+      test_patient = Patient.new({:name => "Mark Jones", :doctor_id => 1, :birthday => "2008-11-11"})
+      expect(test_patient.name()).to(eq("Mark Jones"))
+    end
+  end
+
+  describe('#doctor_id') do
+    it("lets you view the patient's doctor id") do
+      test_patient = Patient.new({:name => "Mark Jones", :doctor_id => 1, :birthday => "2008-11-11"})
+      expect(test_patient.doctor_id()).to(eq(1))
+    end
+  end
+
+  describe('#birthday') do
+    it("lets you view the patient's birthday") do
+      test_patient = Patient.new({:name => "Mark Jones", :doctor_id => 1, :birthday => "2008-11-11"})
+      expect(test_patient.birthday()).to(eq("2008-11-11"))
+    end
+  end
 
 end
